9e64d76d4c68b0691bc71331b884a0687efc7c57
palakshah-47 <palakshah47@gmail.com>
2026-02-20 15:58:58 -0500

    adding Link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { getAllProjects, createProject } from '../services/project'
 import { getInspirationsByProject } from '../services/inspiration'
 import Button from '../components/Button'
